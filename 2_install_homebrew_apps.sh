7febf1762ed03b64b8f575068517f60b3d62f404
Larry Scott <ldscott1970@gmail.com>
2021-06-18 18:32:23 -0400

    adding Bash 5 configs

--- 2_install_homebrew_apps.sh
+++ 2_install_homebrew_apps.sh
@@ -4,3 +4,7 @@ brew install --cask macvim
 brew install ipcalc
 brew install iterm2
 brew install bash-completion
+# Install Bash and set it as the default shell
+brew install bash
+sudo bash -c "echo '/usr/local/bin/bash' >> /etc/shells"
+chsh -s /usr/local/bin/bash
